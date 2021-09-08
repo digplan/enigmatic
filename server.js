2a9494167a58d0622e4925edbe8742c71fad9656
cb <digplan@outlook.com>
2021-09-08 17:31:29 -0500

    updates

--- server.js
+++ server.js
@@ -1,52 +1,6 @@
 const PROTOCOL = require('https')
 const FS = require('fs')
-const READLINE = require('readline')
-
-const DB = {
-    init: ()=> {
-        if(!FS.existsSync('./data.txt'))
-          FS.writeFileSync('./data.txt', `edb ${new Date().toISOString()}`)
-        DB.build()
-    },
-    build: async ()=> {
-        const rl = READLINE.createInterface({
-            input: FS.createReadStream('./data.txt'),
-            crlfDelay: Infinity
-        })
-        for await (const line of rl) {
-            let [time, type, obj] = line.split('\t')
-            if(obj)
-              DB.processLine(type, JSON.parse(obj))
-        }
-    },
-    transaction: (type, obj)=> {
-      if(type == 'GET')
-        return DB.DATA
-      if(obj) obj = JSON.parse(obj)
-      const ret = DB.processLine(type, obj)
-      FS.appendFileSync('./data.txt', `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(obj)}`)
-      return ret
-    },
-    processLine: (type, obj)=> {
-      if(type == 'POST'){
-        const id = +new Date()
-        obj._id = id
-        DB.DATA[id] = obj
-        return DB.DATA[id]
-      }
-      if(type == 'PUT') {
-        DB.DATA[obj._id] = obj
-        return DB.DATA[obj._id]
-      }
-      if(type == 'DELETE'){
-        delete DB.DATA[obj._id]
-        return obj
-      }
-      return;
-    },
-    DATA: {}
-}
-
+const DB = require('./db.js')
 DB.init()
 
 const options = {
