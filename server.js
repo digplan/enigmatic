ca2c3b5c266852de63a4d4ef091a2820ab62fd6e
cb <digplan@outlook.com>
2021-09-08 17:14:07 -0500

    updates

--- server.js
+++ server.js
@@ -1,24 +1,117 @@
-const PROTOCOL = require('http')
+const PROTOCOL = require('https')
 const FS = require('fs')
-const DATA = FS.existsSync('./data.json') ? JSON.parse(FS.readFileSync('./data.json').toString()) : {}
-const SAVE = FS.writeFileSync('./data.json', JSON.stringify(DATA, null, 2))
+const READLINE = require('readline')
 
-//const cert = fs.readFileSync('./digplan.xyz.cert.txt')
-//const key = fs.readFileSync('./digplan.xyz.key.txt')
+const DB = {
+    init: ()=> {
+        if(!FS.existsSync('./data.txt'))
+          FS.writeFileSync('./data.txt', `edb ${new Date().toISOString()}`)
+        DB.build()
+    },
+    build: async ()=> {
+        const rl = READLINE.createInterface({
+            input: FS.createReadStream('./data.txt'),
+            crlfDelay: Infinity
+        })
+        for await (const line of rl) {
+            let [time, type, obj] = line.split('\t')
+            if(obj)
+              DB.processLine(type, JSON.parse(obj))
+        }
+    },
+    transaction: (type, obj)=> {
+      if(type == 'GET')
+        return DB.DATA
+      if(obj) obj = JSON.parse(obj)
+      const ret = DB.processLine(type, obj)
+      FS.appendFileSync('./data.txt', `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(obj)}`)
+      return ret
+    },
+    processLine: (type, obj)=> {
+      if(type == 'POST'){
+        const id = +new Date()
+        obj._id = id
+        DB.DATA[id] = obj
+        return DB.DATA[id]
+      }
+      if(type == 'PUT') {
+        DB.DATA[obj._id] = obj
+        return DB.DATA[obj._id]
+      }
+      if(type == 'DELETE'){
+        delete DB.DATA[obj._id]
+        return obj
+      }
+      return;
+    },
+    DATA: {}
+}
+
+DB.init()
+
+const options = {
+    cert: FS.readFileSync('./localhost.pem'),
+    key: FS.readFileSync('./localhost-key.pem')
+}
 
 const app = (r, s) => {
 
-    if(r.url == '/')
-        return s.end(FS.readFileSync('./index.html').toString())
-    
-    if(r.url == '/index.js')
-        return s.end(FS.readFileSync('./index.js').toString())
+    //console.log([r.method, r.url]);
 
-    if(r.url == '/css.css')
-        return s.end(FS.readFileSync('./css.css').toString())
+    try {
+        
+        if (r.url.match('/api')) {
+            var body = ''
+            r.on('data', (datain) => body += datain)
+            r.on('end', () => {
+                var x = DB.transaction(r.method, body)
+                s.end(JSON.stringify(x))
+            })
+            return;
+        }
 
-    s.end('Not found');
+        if (r.method == 'GET') {
+
+            if (r.url == '/' || r.url == '/index.html')
+                return s.end(FS.readFileSync('./index.html').toString());
+
+            else if (r.url == '/index.js')
+                return s.end(FS.readFileSync('./index.js').toString());
+
+            else if (r.url == '/enigmatic.css')
+                return s.end(FS.readFileSync('./enigmatic.css').toString());
+
+            else if (r.url == '/data')
+                return s.end(JSON.stringify(DATA, null, 2))
+
+            else if (r.url == '/events') {
+                s.writeHead(200, {
+                    'Content-Type': 'text/event-stream',
+                    'Cache-Control': 'no-cache',
+                    Connection: 'keep-alive'
+                })
+
+                var t = setInterval(()=>{
+                    DATA = {time: new Date()}
+                    s.write(`data: ${JSON.stringify(DATA)}`)
+                    s.write('\n\n')
+                    console.log(`SENDING EV`)
+                }, 500)
+
+                s.socket.on('close', function () {
+                    console.log('Client leave')
+                    clearInterval(t)
+                })
+            }
+        }
+
+        if(!r.method.match(/POST|PUT|DELETE/i))
+            return s.end('')
+
+    } catch (e) {
+        s.end('Error ' + e.message);
+    }
 }
 
-const server = PROTOCOL.createServer(app).listen(80)
+const server = PROTOCOL.createServer(options, app).listen(443)
 console.log(server)
