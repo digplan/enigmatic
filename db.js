a3ae72cbcbac35b98442f55b8b6bf856cc2f525a
cb <digplan@outlook.com>
2021-09-08 23:09:29 -0500

    upd

--- db.js
+++ db.js
@@ -2,46 +2,69 @@ const FS = require('fs')
 const READLINE = require('readline')
 
 module.exports = DB = {
-    init: ()=> {
-        if(!FS.existsSync('./data.txt'))
-          FS.writeFileSync('./data.txt', `edb ${new Date().toISOString()}`)
+
+    init: () => {
+
+        if (!FS.existsSync('./data.txt'))
+            FS.writeFileSync('./data.txt', `edb ${new Date().toISOString()}`)
         DB.build()
+
     },
-    build: async ()=> {
+
+    build: async () => {
+
         const rl = READLINE.createInterface({
             input: FS.createReadStream('./data.txt'),
             crlfDelay: Infinity
         })
+
         for await (const line of rl) {
             let [time, type, obj] = line.split('\t')
-            if(obj)
-              DB.processLine(type, JSON.parse(obj))
+            if (obj)
+                DB.processLine(type, JSON.parse(obj))
         }
+
     },
-    transaction: (type, obj)=> {
-      if(type == 'GET')
-        return DB.DATA
-      if(obj) obj = JSON.parse(obj)
-      const ret = DB.processLine(type, obj)
-      FS.appendFileSync('./data.txt', `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(obj)}`)
-      return ret
+
+    transaction: (type, obj) => {
+
+        if (type == 'GET')
+            return DB.DATA
+
+        if (obj) obj = JSON.parse(obj)
+        const ret = DB.processLine(type, obj)
+        FS.appendFileSync('./data.txt', `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(obj)}`)
+        return ret
+
     },
-    processLine: (type, obj)=> {
-      if(type == 'POST'){
-        const id = +new Date()
-        obj._id = id
-        DB.DATA[id] = obj
-        return DB.DATA[id]
-      }
-      if(type == 'PUT') {
-        DB.DATA[obj._id] = obj
-        return DB.DATA[obj._id]
-      }
-      if(type == 'DELETE'){
-        delete DB.DATA[obj._id]
-        return obj
-      }
-      return;
+
+    processLine: (type, arr) => {
+
+        let ret = []
+
+        for (const obj of arr) {
+
+            if (type == 'POST') {
+                const id = +new Date()
+                obj._id = id
+                DB.DATA[id] = obj
+                ret.push(DB.DATA[id])
+            }
+            if (type == 'PUT') {
+                DB.DATA[obj._id] = obj
+                ret.push(DB.DATA[obj._id])
+            }
+            if (type == 'DELETE') {
+                delete DB.DATA[obj._id]
+                ret.push(obj)
+            }
+
+        }
+
+        return ret
+
     },
+
     DATA: {}
+
 }
