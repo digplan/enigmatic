e0af3864d0c1e9f3bf38e5cfc27142c38e82d85a
cb <digplan@outlook.com>
2021-10-06 21:26:10 -0500

    updates

--- db.js
+++ db.js
@@ -1,85 +1,109 @@
 /*
+  db.js
+  
+  Include:
+  const DB = require('./db.js')
 
-.init(version)
-
-DB.DATA = [{_id:'sdkjshdkjasd', _type:'tablename'}, {_id:'asdaaaasdadd', _type:'tablename'}]
-GET /api/i._type=="person"
-POST /api   [{_type:'tablename'}, {_type:'tablename'}]
-PUT /api    [{_id:'sdkjshdkjasd', _type:'tablename'}, {_id:'sdkjshdkjasd', _type:'tablename'}]
-DELETE /api [{_id:'sdkjshdkjasd'}, {_id:'widhgahsgdjasd']
+  Tests:
+  > node db.js
 
 */
 
-const DB = {
+const FS = require('fs')
+let mDB = { DATA: [] }
 
-    init: async (version) => {
+exports.dbinit = async (fn = './data.txt', version) => {
 
-        if (!require('fs').existsSync('./data.txt')) {
-            const keys = require('./crypto.js').createKeys()
-            require('fs').writeFileSync('./data.txt', `edb version 0.0.1`)
-            console.log(`edb public key is ${keys.public_compressed}`)
-            console.log(`edb private key is ${keys.private}`)
-            DB.transaction('POST', `[{"_type": "_identity", "name": "edbroot", "public": "${keys.public_compressed}"}]`);
-        }
+    mDB.filename = fn
 
-        const rl = require('readline').createInterface({
-            input: require('fs').createReadStream('./data.txt'),
-            crlfDelay: Infinity
-        })
+    if (!FS.existsSync(fn)) {
+        const keys = require('./crypto.js').createKeys()
+        require('fs').writeFileSync(fn, `edb 0.0.1`)
+        console.log(`edb public key is ${mDB.public = keys.public_compressed}`)
+        console.log(`edb private key is ${keys.private}`)
+        exports.transaction('POST', `[{"_type": "_identity", "name": "edbroot", "public": "${keys.public_compressed}"}]`);
+    }
 
-        for await (const line of rl) {
-            let [time, type, obj] = line.split('\t')
-            if(!obj)
-              continue
-            if (!version || (new Date(time) <= new Date(version)))
-              DB.readLine(time, type, JSON.parse(obj))
-        }
+    const rl = require('readline').createInterface({
+        input: require('fs').createReadStream(fn),
+        crlfDelay: Infinity
+    })
 
-        console.log(JSON.stringify(DB.DATA, null, 2))
-        const ff = DB.query('i._type=="person"')
-        console.log(ff)
+    for await (const line of rl) {
+        let [time, type, obj] = line.split('\t')
+        if (obj && (!version || (new Date(time) <= new Date(version))))
+            exports.readLine(time, type, JSON.parse(obj))
+    }
 
-    },
-
-    query: (s) => {
-        return DB.DATA.filter(new Function('i', `return ${s}`))
-    },
-
-    transaction: (type, arr) => {
-
-        let ret = []
-
-        for (rec of JSON.parse(arr)) {
-            ret.push(DB.readLine(+new Date(), type, rec))
-            require('fs').appendFileSync('./data.txt', `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(rec)}`)
-        }
-
-        return ret
-
-    },
-
-    readLine: (time, type, obj) => {
-
-        //for (const obj of arr) {
-
-            if (type == 'POST') {
-                DB.DATA.push(obj)
-            } else if (type == 'PUT') {
-                for (var i = 0; i < DB.DATA.length; i++) {
-                    if (DB.DATA[i]._id == obj._id) {
-                        DB.DATA[i] = obj
-                    }
-                }
-            } else if (type == 'DELETE') {
-                DB.DATA = DB.DATA.filter(i => i._id != obj._id)
-            }
-
-        //}
-
-    },
-
-    DATA: []
+    return mDB
 
 }
 
-module.exports = DB
+exports.query = (s) => {
+    return { query: s, results: mDB.DATA.filter(s) }
+}
+
+exports.transaction = (type, arr) => {
+    if (typeof arr == 'string')
+        arr = JSON.parse(arr)
+
+    let ret = []
+
+    for (rec of arr) {
+        ret.push(exports.readLine(+new Date(), type, rec))
+        FS.appendFileSync(mDB.filename, `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(rec)}`)
+    }
+
+    return ret
+}
+
+exports.readLine = (time, type, obj) => {
+    if (type == 'POST') {
+        mDB.DATA.push(obj)
+    } else if (type == 'PUT') {
+        for (var i = 0; i < DB.DATA.length; i++) {
+            if (mDB.DATA[i]._id == obj._id) {
+                mDB.DATA[i] = obj
+            }
+        }
+    } else if (type == 'DELETE') {
+        mDB.DATA = mDB.DATA.filter(i => i._id != obj._id)
+    }
+}
+
+exports.getToken = (idIdentity) => {
+
+}
+
+exports.newIdentity = () => { }
+
+exports.newRole = () => { }
+
+exports.grant = (idFrom, idTo, definition) => {
+
+}
+
+/*****************
+       Tests  
+******************/
+
+if (require.main !== module)
+    return
+
+test()
+
+async function test() {
+    console.log('*** Testing db.js')
+
+    const { dbinit, query, transaction, getToken, newIdentity, newRole, grant } = require('./db.js')
+
+    const filename = `./TESTING-eDB.txt`;
+    if (FS.existsSync(filename))
+        FS.unlinkSync(filename)
+
+    const DB = await dbinit(filename)
+    console.log(DB)
+    console.log(`db is located at ${DB.filename} and has ${DB.DATA.length} records`)
+    const root = query(new Function('i', 'return i._type=="_identity" && i.name=="edbroot"'))
+    console.log(root)
+}
