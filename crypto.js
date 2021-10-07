40e38a6d86776c5cc2112998d103b401f163ceec
CB <digplan@outlook.comm>
2021-10-07 00:01:12 -0500

    updates

--- crypto.js
+++ crypto.js
@@ -2,77 +2,102 @@
   crypto.js
   
   Include:
-  const {createKeys, sign, verify, hash} = require('./crypto.js')
+  const CRYPTO = require('./crypto.js')
+  const crypto = new CRYPTO()
 
   Tests:
   > node crypto.js
 
 */
 
-const {createHash, createECDH, createSign, createVerify, randomBytes, createCipheriv, createDecipheriv} = require('crypto')
+const { createHash, createECDH, createSign, createVerify, randomBytes, createCipheriv, createDecipheriv } = require('crypto')
 
-exports.privateKey = null;
-exports.publicKey = null;
-exports.publicKeyCompressed = null;
-exports.pem = null;
+class CRYPTO {
 
-exports.createKeys = (privateHex) => {
-        const ecdh = createECDH('secp256k1')
-        privateHex ? ecdh.setPrivateKey(privateHex, 'hex') : ecdh.generateKeys()
-        var pemformat = `308201510201010420${ecdh.getPrivateKey('hex')}a081e33081e0020101302c06072a8648ce3d0`
-        pemformat += `101022100fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f3044042000000`
-        pemformat += `00000000000000000000000000000000000000000000000000000000000042000000000000000000000000`
-        pemformat += `0000000000000000000000000000000000000000704410479be667ef9dcbbac55a06295ce870b07029bfcd`
-        pemformat += `b2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d`
-        pemformat += `4b8022100fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141020101a144034`
-        pemformat += `200${ecdh.getPublicKey('hex')}`
-        const b64 = Buffer.from(pemformat, 'hex')
+        constructor (privateHex) {
+                const ecdh = createECDH('secp256k1')
+                privateHex ? ecdh.setPrivateKey(privateHex, 'hex') : ecdh.generateKeys()
+                var pemformat = `308201510201010420${ecdh.getPrivateKey('hex')}a081e33081e0020101302c06072a8648ce3d0`
+                pemformat += `101022100fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f3044042000000`
+                pemformat += `00000000000000000000000000000000000000000000000000000000000042000000000000000000000000`
+                pemformat += `0000000000000000000000000000000000000000704410479be667ef9dcbbac55a06295ce870b07029bfcd`
+                pemformat += `b2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d`
+                pemformat += `4b8022100fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141020101a144034`
+                pemformat += `200${ecdh.getPublicKey('hex')}`
+                const b64 = Buffer.from(pemformat, 'hex')
 
-        let keys = {}
-        keys.pem = `-----BEGIN EC PRIVATE KEY-----\n${b64.toString('base64')}\n-----END EC PRIVATE KEY-----`
-        keys.public = ecdh.getPublicKey('hex')
-        keys.public_compressed = ecdh.getPublicKey('hex', 'compressed')
-        keys.private = ecdh.getPrivateKey('hex')
-        return keys
-}
-  
-exports.sign = (s) => {
-        const signer = createSign('SHA256')
-        signer.update(s)
-        return signer.sign(exports.pem).toString('hex')
-}
-  
-exports.verify = (s, sig) => {
-        if(!sig) throw Error('enter a signature')
-        const verify = createVerify('SHA256') 
-        verify.update(s)
-        return verify.verify(this.pem, Buffer.from(sig.toString('hex'), 'hex'))
-}
-  
-exports.hash = (s) => createHash('sha256').update(s).digest('hex')
+                this.pem = `-----BEGIN EC PRIVATE KEY-----\n${b64.toString('base64')}\n-----END EC PRIVATE KEY-----`
+                this.public = ecdh.getPublicKey('hex')
+                this.public_compressed = ecdh.getPublicKey('hex', 'compressed')
+                this.private = ecdh.getPrivateKey('hex')
+                return this
+        }
 
-exports.encrypt = (s, key, iv = randomBytes(32)) => {
-        const cipher = createCipheriv('aes-256-cbc', key, iv)
-        let encryptedData = cipher.update(s, 'utf-8', 'hex')
-        return {iv: iv, encrypted: cipher.final("hex")}
+
+        sign (text) {
+                const signer = createSign('SHA256')
+                signer.update(text)
+                return signer.sign(this.pem).toString('hex')
+        }
+
+        verify (text, signature) {
+                if (!signature) throw Error('enter a signature')
+                const verify = createVerify('SHA256')
+                verify.update(text)
+                return verify.verify(this.pem, Buffer.from(signature))
+        }
+
+        hash (s) {
+                return createHash('sha256').update(s).digest('hex')
+        }
+
+        encrypt (text, key = this.private, iv = randomBytes(16)) {
+                key = Buffer.from(key).slice(32)
+                console.log(iv)
+                const cipher = createCipheriv('aes-256-cbc', key, iv)
+                cipher.update(text, 'utf-8', 'hex')
+                return iv.toString('hex') + ':' + cipher.final("hex")
+        }
+
+        decrypt (ivtext) {
+                const [siv, s] = ivtext.split(':')
+                const key = Buffer.from(this.private).slice(32)
+                console.log(Buffer.from(siv, 'hex'))
+                const cipher = createDecipheriv('aes-256-cbc', key, Buffer.from(siv, 'hex'))
+                cipher.update(s, 'utf-8', 'hex')
+                return cipher.final('hex')
+        }
+
 }
 
-exports.decrypt = (s, key, iv) => {
-        const cipher = createDecipheriv('aes-256-cbc', key, iv)
-        let encryptedData = cipher.update(s, 'utf-8', 'hex')
-        return cipher.final("hex")
-}
+module.exports = CRYPTO
 
 /*****************
        Tests  
 ******************/
 
-if (require.main !== module)
-    return
+if (require.main === module)
+        tests()
 
-const {createKeys, sign, verify, hash, encrypt, decrypt} = require('./crypto.js')
-const newKeys = createKeys()
-console.log(newKeys)
+function tests() {
 
-let encryptedData = encrypt('mc', newKeys.private)
-console.log("Encrypted message: " + encryptedData);
+        const CRYPTO = require('./crypto.js');
+
+        const crypto = new CRYPTO()
+        console.log(`${JSON.stringify(crypto)}`)
+
+        const sig = crypto.sign('this message')
+        console.log(`signing message. sig = ${sig}`)
+
+        const verified = crypto.verify('this message', sig)
+        console.log(`is verified: ${verified}`)
+
+        const hashed = crypto.hash('cb')
+        console.log(`hash a value: ${hashed}`)
+
+        let encryptedData = crypto.encrypt('text to hide')
+        console.log(`encrypt some data ${encryptedData}`)
+
+        //let deryptedData = crypto.decrypt(encryptedData)
+        //console.log(decryptedData)
+}
--- db.js
+++ db.js
@@ -3,6 +3,7 @@
   
   Include:
   const DB = require('./db.js')
+  const db = new DB(filename, version)
 
   Tests:
   > node db.js
@@ -10,100 +11,109 @@
 */
 
 const FS = require('fs')
-let mDB = { DATA: [] }
 
-exports.dbinit = async (fn = './data.txt', version) => {
+class DB {
 
-    mDB.filename = fn
+    DATA = []
 
-    if (!FS.existsSync(fn)) {
+    constructor (filename) {
+        if (FS.existsSync(fn))
+            return this
+
         const keys = require('./crypto.js').createKeys()
-        require('fs').writeFileSync(fn, `edb 0.0.1`)
+        FS.writeFileSync(fn, `edb 0.0.1`)
         console.log(`edb public key is ${mDB.public = keys.public_compressed}`)
         console.log(`edb private key is ${keys.private}`)
-        exports.transaction('POST', `[{"_type": "_identity", "name": "edbroot", "public": "${keys.public_compressed}"}]`);
+        this.transaction('POST', `[{"_type": "_identity", "name": "edbroot", "public": "${keys.public_compressed}"}]`)
     }
 
-    const rl = require('readline').createInterface({
-        input: require('fs').createReadStream(fn),
-        crlfDelay: Infinity
-    })
+    async init () {
+        const rl = require('readline').createInterface({
+            input: require('fs').createReadStream(fn),
+            crlfDelay: Infinity
+        })
 
-    for await (const line of rl) {
-        let [time, type, obj] = line.split('\t')
-        if (obj && (!version || (new Date(time) <= new Date(version))))
-            exports.readLine(time, type, JSON.parse(obj))
-    }
-
-    return mDB
-
-}
-
-exports.query = (s) => {
-    return { query: s, results: mDB.DATA.filter(s) }
-}
-
-exports.transaction = (type, arr) => {
-    if (typeof arr == 'string')
-        arr = JSON.parse(arr)
-
-    let ret = []
-
-    for (rec of arr) {
-        ret.push(exports.readLine(+new Date(), type, rec))
-        FS.appendFileSync(mDB.filename, `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(rec)}`)
-    }
-
-    return ret
-}
-
-exports.readLine = (time, type, obj) => {
-    if (type == 'POST') {
-        mDB.DATA.push(obj)
-    } else if (type == 'PUT') {
-        for (var i = 0; i < DB.DATA.length; i++) {
-            if (mDB.DATA[i]._id == obj._id) {
-                mDB.DATA[i] = obj
-            }
+        for await (const line of rl) {
+            let [time, type, obj] = line.split('\t')
+            if (obj && (!version || (new Date(time) <= new Date(version))))
+                this.readLine(time, type, JSON.parse(obj))
         }
-    } else if (type == 'DELETE') {
-        mDB.DATA = mDB.DATA.filter(i => i._id != obj._id)
     }
-}
 
-exports.getToken = (idIdentity) => {
+    query (s) {
+        return { query: s, results: mDB.DATA.filter(s) }
+    }
 
-}
+    transaction (type, arr) {
+        if (typeof arr == 'string')
+            arr = JSON.parse(arr)
 
-exports.newIdentity = () => { }
+        let ret = []
 
-exports.newRole = () => { }
+        for (rec of arr) {
+            ret.push(exports.readLine(+new Date(), type, rec))
+            FS.appendFileSync(mDB.filename, `\r\n${new Date().toISOString()}\t${type}\t${JSON.stringify(rec)}`)
+        }
 
-exports.grant = (idFrom, idTo, definition) => {
+        return ret
+    }
 
+    readLine (time, type, obj) {
+        if (type == 'POST') {
+            mDB.DATA.push(obj)
+        } else if (type == 'PUT') {
+            for (var i = 0; i < DB.DATA.length; i++) {
+                if (mDB.DATA[i]._id == obj._id) {
+                    mDB.DATA[i] = obj
+                }
+            }
+        } else if (type == 'DELETE') {
+            mDB.DATA = mDB.DATA.filter(i => i._id != obj._id)
+        }
+    }
+
+    getToken (idIdentity) {
+
+    }
+
+    newIdentity () { }
+
+    newRole () { }
+
+    newIdentityRole () {}
+
+    grant (idFrom, idTo, definition) {
+
+    }
 }
 
 /*****************
        Tests  
 ******************/
 
-if (require.main !== module)
-    return
+if (require.main === module)
+    tests()
 
-test()
+async function tests() {
 
-async function test() {
-    console.log('*** Testing db.js')
+    console.log('testing')
 
-    const { dbinit, query, transaction, getToken, newIdentity, newRole, grant } = require('./db.js')
+    if (FS.existsSync(`./TESTING-eDB.txt`))
+        FS.unlinkSync(`./TESTING-eDB.txt`)
 
-    const filename = `./TESTING-eDB.txt`;
-    if (FS.existsSync(filename))
-        FS.unlinkSync(filename)
-
-    const DB = await dbinit(filename)
-    console.log(DB)
+    const DB = require('./db.js')
+    const db = new DB(`./TESTING-eDB.txt`)
+    await db.init()
     console.log(`db is located at ${DB.filename} and has ${DB.DATA.length} records`)
-    const root = query(new Function('i', 'return i._type=="_identity" && i.name=="edbroot"'))
-    console.log(root)
+
+    const newid = db.newIdentity('chris')
+    const newrole = db.newRole('users')
+    const newidrole = db.newIdentityRole('chris', 'users')
+    const grant = db.grant('users', '_type=="users"')
+    const id = db.transaction('POST', {_type:'fruit', name:'apples'})ß
+    const txPut = db.transaction('PUT', {_id: id, color: 'red'})
+    const idd = db.transaction('POST', {_type:'fruit', name:'orange'})
+    const txDelete = db.transaction('DELETE', {_id: idd})
+    const root = db.query(new Function('i', 'return i._type=="fruit"'))
+    
 }
