0f7a4d60055fd9f493fb55532145ff7d357dffb7
CB <digplan@outlook.comm>
2021-10-27 23:38:49 -0500

    upda

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 window.$ = document.querySelector.bind(document)
 window.$$ = document.querySelectorAll.bind(document)
-window.body = document.body;
-window.DataSources = {};
+window.body = document.body
+
+window.DataSources = {}
 
 window.load = src => {
   return new Promise((r, j)=>{
@@ -16,15 +17,15 @@ window.DATA = new Proxy(
   {},
   {
     set: (obj, prop, value) => {
-      obj[prop] = value;
+      obj[prop] = value
       $$(`[data^=${prop}]`).forEach(control => {
-        if (control.set) control.set(value);
-        else control.innerHTML = value;
-      });
+        if (control.set) control.set(value)
+        else control.innerHTML = value
+      })
       return prop;
     },
   }
-);
+)
 
 class EnigmaticElement extends HTMLElement {
   constructor() {
@@ -32,70 +33,70 @@ class EnigmaticElement extends HTMLElement {
     for (let attr of Array.from(this.attributes)) this[attr.name] = attr.value;
   }
   toggle() {
-    this.classList.toggle('hide');
-    this.classList.toggle('show');
+    this.classList.toggle('hide')
+    this.classList.toggle('show')
   }
   set(s) {
-    this.innerHTML = s;
+    this.innerHTML = s
   }
 }
 
 class MapEmbed extends EnigmaticElement {
   constructor() {
-    super();
+    super()
     this.innerHTML = `<iframe height='100%' width=100% frameborder=0 src="https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=${
       this.getAttribute('where') || 'Chicago'
-    }&output=embed"></iframe>`;
+    }&output=embed"></iframe>`
   }
 }
 
 class YouTube extends EnigmaticElement {
   constructor() {
-    super();
+    super()
     this.innerHTML = `<embed src='//www.youtube.com/embed/${
       this.getAttribute('id') || 'MlDx9s-zJMM'
-    }?rel=0&amp;controls=0&amp;showinfo=0' style='height:100%;width:100%' />`;
+    }?rel=0&amp;controls=0&amp;showinfo=0' style='height:100%;width:100%' />`
   }
 }
 
 class View extends EnigmaticElement {
   constructor() {
     super();
-    if (this.agent && !navigator.userAgent.match(this.agent)) this.remove();
+    if (this.agent && !navigator.userAgent.match(this.agent)) this.remove()
   }
   showOnly() {
-    let visible = $$('view-panel:not([class="hide"])');
-    for (let e of visible) e.toggle();
-    this.toggle();
+    let visible = $$('view-panel:not([class="hide"])')
+    for (let e of visible) e.toggle()
+    this.toggle()
   }
 }
 
 class DataSource {
   constructor() {
-    [this.dataclass, this.datasrc] = body.getAttribute('datasrc').split(':');
+    [this.dataclass, this.datasrc] = body.getAttribute('datasrc').split(':')
   }
 }
 
 window.DataLocalStorage = class DataLocalStorage extends DataSource {
   constructor() {
-    super();
-    console.log(`loaded ${this.dataclass} ${this.datasrc}`);
+    super()
+    console.log(`loaded ${this.dataclass} ${this.datasrc}`)
     if (!localStorage.getItem('enigmatic'))
-      localStorage.setItem('enigmatic', 'v0.2.0');
-    DATA['_version'] = localStorage.getItem('enigmatic');
+      localStorage.setItem('enigmatic', 'v0.2.0')
+    DATA['_version'] = localStorage.getItem('enigmatic')
   }
-};
+}
 
 window.DataEventSource = class DataEventSource extends DataSource {
   constructor() {
-    super();
-    const es = new EventSource(this.datasrc);
+    super()
+    const es = new EventSource(this.datasrc)
     es.onmessage = ({ data: d }) => {
-      const obj = JSON.parse(d);
+      const obj = JSON.parse(d)
       DATA['time'] = obj.time;
-    };
+    }
   }
-};
+}
 
 customElements.define('map-embed', MapEmbed);
 customElements.define('youtube-video', YouTube);
