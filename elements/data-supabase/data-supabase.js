d46261035662f18c7e6979334709baa13a27c54e
CB <digplan@outlook.comm>
2022-01-15 16:06:12 -0600

    upd

--- elements/data-supabase/data-supabase.js
+++ elements/data-supabase/data-supabase.js
@@ -1,12 +1,17 @@
 class DataSupabase extends EnigmaticElement {
     attr = {}
+    url = ''
     constructor() { 
         super()
     }
     async connectedCallback() {
         for (let attr of this.attributes)
             this.attr[attr.name] = attr.value
-        this.attr.key = window.supa_anon
+        this.headers = {
+            'apikey': window.supa_anon,
+            'Authorization': `Bearer ${window.supa_anon}`
+        }
+        this.url = `https://${db}.supabase.co/rest/v1/${t}`
         if (!this.hasAttribute('wait')) {
             console.log(this.id)
             await window.ready()
@@ -14,19 +19,29 @@ class DataSupabase extends EnigmaticElement {
         }
     }
     async fetch() {
-        const {href, key, target, cache} = this.attr
-        let hdrs = {
-            'apikey': key,
-            'Authorization': `Bearer ${key}`
-        }, json
+        const {db, t, q, key, target, cache} = this.attr
+        let json
+        const href = `${this.url}?${q}`
         if (cache)
             json = await this.cache(href)
         else
-            json = await (await fetch(href, {headers: hdrs})).json()
-        if(json.message)
-            throw Error(json.message)
-        if (target)
-            window.data[target] = json
+            json = await (await fetch(href, {headers: this.headers})).json()
+        if(json.message) throw Error(json.message)
+        if (target) window.data[target] = json
+    }
+    async insert(method = 'POST') {
+        let fields = $([target=`${this.id}`]), o = {}
+        for(const f in fields)
+            o[f.name] = f.value
+        const p = {method: method, body: JSON.stringify(o), headers: this.headers}
+        let json = await (await fetch(`${this.url}`, p)).json()
+        if (json.message) throw Error(json.message)
+    }
+    async update() {
+        return this.insert('PATCH')
+    }
+    async delete() {
+        return this.insert('DELETE')
     }
     async cache(url) {
         const cached = localStorage.getItem(url)
