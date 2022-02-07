06a21666cf978d75ceb896b7a749af4bf1fa5fd9
cb <digplan@outlook.com>
2022-02-07 11:18:23 -0600

    add module

---
+++ node_modules/supramap/supramap.mjs
@@ -0,0 +1,72 @@
+import fs from 'node:fs'
+import BaseType from './basetype.mjs'
+import Types from './types.mjs'
+
+class Supramap extends Map {
+
+    types = Types
+
+    async loadFunctions() {
+        let dirname = new URL(import.meta.url).pathname.split('/').slice(0, -1).join('/').slice(1)
+        if (process.platform !== 'win32')
+            dirname = '/' + dirname
+        this.functions = {}
+        for (const model of fs.readdirSync(dirname + '/functions')) {
+            if (!model.endsWith('.mjs')) continue
+            let furl = 'file://' + dirname + '/functions/' + model
+            console.log('loading function: ' + furl)
+            let f = await import(furl);
+            this.functions[model.replace('.mjs', '')] = f.default;
+        }
+    }
+
+    query(q) {
+        const arr = []
+        for (const [_, v] of this) {
+            if(q(v))
+                arr.push(v)
+        }
+        return arr
+    }
+
+    set(o) {
+        if (!Array.isArray(o))
+            o = [o]
+        o = o.map(item => {
+            const newrec = eval(`new ${this.types[item.id.split(':')[0]]}()`)
+            newrec.validate(item)
+            if (this.has(newrec.id))
+                newrec._created = this.get(newrec.id)._created
+            return newrec
+        })
+        for (const rec of o) {
+            super.set(rec.id, rec)
+        }
+    }
+
+    json() {
+        const ret = {}
+        for (const [k, v] of this) {
+            ret[k] = v
+        }
+        return JSON.stringify(ret, null, 2)
+    }
+
+    save(filename) {
+        const fp = new URL(filename, import.meta.url).pathname.slice(1)
+        fs.writeFileSync(fp, this.json())
+        return { filename: fp, records: this.size }
+    }
+
+    load(filename) {
+        const fp = new URL(filename, import.meta.url).pathname.slice(1)
+        const data = JSON.parse(fs.readFileSync(fp))
+        for (let rec in data) {
+            super.set(rec, data[rec])
+        }
+        return { filename: fp, records: this.size }
+    }
+
+}
+
+export { Supramap }
---
+++ node_modules/supramap/types.mjs
@@ -0,0 +1,85 @@
+import BaseType from './basetype.mjs'
+
+class entity extends BaseType {
+    // age = 40  // default value
+    // Street // required value
+}
+
+class person extends entity {
+    firstname
+    middlename
+    lastname
+    nickname
+    email
+    phone
+    picture
+}
+
+class employee extends person {
+    employeenumber
+    title
+    department
+    company
+    hiredate
+    termdate
+    manager
+    worktype
+}
+
+class group extends entity {
+    name
+    description
+}
+
+class groupmember extends BaseType {
+    group
+    member
+}
+
+class timezone extends BaseType {
+    name
+    abbr
+    offset
+}
+
+class location extends entity {
+    name
+    address
+    city
+    state
+    zip
+    country
+    lat
+    lng
+    timezone
+}
+
+class keypair {
+    name
+    public
+    private
+    type
+}
+
+class country {
+    name
+    abbr
+    code
+}
+
+class language {
+    name
+}
+
+export default {
+    "entity": entity,
+    "person": person,
+    "employee": employee,
+    "group": group,
+    "groupmember": groupmember,
+    "timezone": timezone,
+    "location": location,
+    "keypair": keypair,
+    "country": country,
+    "language": language
+}
