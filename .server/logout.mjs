af9640cd9b3be0b83a97db33dba7853fcd919818
cb <digplan@outlook.com>
2022-02-08 10:11:22 -0600

    new branch

--- .server/logout.mjs
+++
@@ -1,3 +0,0 @@
-export default (r, s, data) => {
-    return s.setHeader('Set-Cookie', 'token=; HttpOnly; Secure; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT').end('logged out')
-}
--- .server/now.mjs
+++
@@ -1,3 +0,0 @@
-export default (r, s, data) =>{
-  return s.endJSON({time: new Date().toISOString()})
-}
