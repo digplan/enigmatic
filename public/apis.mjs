8d2ff4249929e3ab4ccc0250e65319ac01837487
CB <digplan@outlook.comm>
2022-02-08 02:40:29 -0600

    updates

--- public/apis.mjs
+++ public/apis.mjs
@@ -1,3 +1,10 @@
-export default {
+const api = {
     "randomusers": {"url": "https://randomuser.me/api/?results=${n}"}
 }
+const html = (s) => {
+
+}
+
+html`https://randomuser.me/api/?results=3, {n:3}, <div>${first_name}</div>`
+
+element`my-element, <div>${first_name}</div>`
