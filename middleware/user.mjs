c27342cfa378fb1adbd943e6e28996ac810fb8d2
CB <digplan@outlook.comm>
2022-02-13 23:08:56 -0600

    fix

--- middleware/user.mjs
+++ middleware/user.mjs
@@ -1,14 +1,16 @@
-const hash = (s)=> {
+const hash = async (s) => {
     const buf = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(s))
     return Array.from(new Uint8Array(buf)).map(b => b.toString(16).padStart(2, '0')).join('')
 }
 
-export default (r, s, data) => {
+export default async (r, s, data) => {
     if (r.user) return
-    const [type, userpass] = r.headers.authorization.split(' ')
-    const [user, pass] = Buffer.from(userpass, 'base64').toString('ascii').split(':')
-    const hashed = hash(hash(pass))
-    const pass = r.server.db[`user:${user}`].pass
-    if (!pass || hashed !== pass) return
-    r.user = user
+    const { authorization, cookie } = r.headers
+    if (authorization) {
+        const [type, userpass] = authorization.split(' ')
+        const [user, pass] = Buffer.from(userpass, 'base64').toString('ascii').split(':')
+        const hashed = await hash(await hash(pass))
+        if (hashed !== r.server.db[`user:${user}`].pass) return
+        r.user = user
+    }
 }
