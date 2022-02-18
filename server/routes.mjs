1fa12ee5f05e2baf4b2125fc8e817d95c1f066df
cb <digplan@outlook.com>
2022-02-17 19:37:25 -0600

    u

--- server/routes.mjs
+++ server/routes.mjs
@@ -1,32 +1,31 @@
 import Util from './util.mjs'
 const util = new Util()
 
+const { JWT } = import('jwt-tiny')
+const jwt = new JWT('your-256-bit-secret')
+
 export default {
 
     _debug: (r, s, data) => {
         console.log(`Headers: ${JSON.stringify(r.headers, null, 2)}`, `Method: ${r.method}`, `URL: ${r.url}`, `Data: ${JSON.stringify(data, null, 2)}`)
     },
 
-    _db: (r, s, data) => {
-        if (r.server.db) return false
-        if (existsSync(path)) {
-            r.server.db = JSON.parse(readFileSync(path, 'utf8'))
-        } else {
-            r.server.db = defaultDB
-            writeFileSync(path, JSON.stringify(r.server.db, null, 2))
-        }
+    _user: (r, s, data, server, db) => {
+        if (!r.headers.Authorization) return
+        const jwt = jwt.verify(r.headers.Authorization.split(' ')[1])
+        console.log(jwt);  //if(jwt.name) r.user = jwt.name
     },
 
-    stats: (r, s, data) => {
+    stats: (r, s) => {
         return s.endJSON(util.stats())
     },
 
-    api: (r, s, data) => {
-
+    api: (r, s, data, server, db) => {
+        if(!r.user) return s.writeHead(401).end()
+        console.log(r.user)
     },
 
-    token: () => {
-        if (r.user) return
+    token: (r, s) => {
         const { authorization, cookie } = r.headers
         if (authorization) {
             const [type, userpass] = authorization.split(' ')
@@ -35,7 +34,7 @@ export default {
             if (hashed !== r.server.db[`user:${user}`].pass) return
             r.user = user
         }
-        return false
+        return s.writeHead(401).end()
     }
 
 }
--- server/server.mjs
+++ server/server.mjs
@@ -4,8 +4,6 @@ import { Server } from 'node:https';
 let dir = process.cwd()
 let dir_key = `${dir}/../keys/key.pem`
 let dir_cert = `${dir}/../keys/cert.pem`
-let dir_middleware = `file://${dir}/middleware`
-let dir_routes = `file://${dir}/routes`
 
 class HTTPSServer extends Server {
     middleware = []
@@ -28,23 +26,16 @@ class HTTPSServer extends Server {
                         s.end(JSON.stringify(obj))
                     }
                     try { data = JSON.parse(data) } catch (e) { }
-                    r.server = this
-                    this.middleware.some((f) => {
-                        if (this.debug) console.log(`middleware: ${f.name}`)
-                        return f(r, s, data)
-                    })
                     if (r.url == '/') r.url = '/index.mjs'
+
+                    for(const m of this.middleware)
+                        this.middleware[m](r, s, data, server, this.db)
+
                     const route = this.routes['/' + r.url.split('/')[1]]
-                    if (this.debug) console.log(`route: ${r.url}`)
                     if (!route)
-                        return this.routes['/404'](r, s, data)
-                    if (route.toString().startsWith('class')) {
-                        const f = new route(), method = r.method.toLowerCase()
-                        if (!f[method])
-                            return s.writeHead(405).end()
-                        return f[method](r, s, data)
-                    }
-                    return route(r, s, data)
+                        return s.writeHead(404).end()
+
+                    return route(r, s, data, server, this.db)
                 })
             } catch (e) {
                 console.log(e)
@@ -54,39 +45,11 @@ class HTTPSServer extends Server {
         )
     }
     async getMiddleware() {
-        const mfiles = []
-        if (existsSync('./middleware')) {
-            for (const model of readdirSync('./middleware'))
-                mfiles.push(model)
-            mfiles.sort((a, b) => a.localeCompare(b))
-            for (const model of mfiles) {
-                if (!model.endsWith('.mjs')) continue
-                let f = await import(`${dir_middleware}/${model}`)
-                this.middleware.unshift(f.default)
-            }
-        }
-        if (existsSync('./routes')) {
-            for (const model of readdirSync('./routes')) {
-                if (!model.endsWith('.mjs')) continue
-                const name = model.split('.')[0]
-                let f = (await import(`${dir_routes}/${model}`)).default
-                this.routes[`/${name}`] = f
-            }
-        }
-        if (existsSync('./routes.mjs')) {
-            let f = await import('../routes.mjs')
-            for (const midf in f.default) {
-                midf.startsWith('_') ? this.middleware[midf] = f.default[midf] : this.routes[`/${midf}`] = f.default[midf]
-            }
+        let f = await import('routes.mjs')
+        for (const midf in f.default) {
+            midf.startsWith('_') ? this.middleware[midf] = f.default[midf] : this.routes[`/${midf}`] = f.default[midf]
         }
     }
 }
 
-class Route {
-    constructor(r, s, data) { }
-    route(r, s, data) {
-        this[r.method](r, s, data)
-    }
-}
-
-export { HTTPSServer, Route }
+export { HTTPSServer }
