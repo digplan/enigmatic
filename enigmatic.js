1ff49d94b6865318a725f1d50a72ef84e6a20c86
CB <digplan@outlook.comm>
2022-09-28 20:54:11 -0500

    0.11.2 flatten

--- enigmatic.js
+++ enigmatic.js
@@ -33,8 +33,23 @@ w.ready = async () => {
   })
 }
 
-/////// Custom element
+// Template a string, using {$key}, {_key_}, {_val_}
+// ie.. {key1: {name: 'value'}, key2: {name: 'value2'}} OR [{name: 'value'}, {name: 'value2'}]
+w.flatten = (obj, text) => {
+  let htmls = ''
+  if (obj instanceof Array) obj = { ...obj }
+  for (let k in obj) {
+    let html = text.replaceAll('{$key}', k)
+    for (let j in obj[k]) {
+      html = html.replaceAll('{_key_}', j).replaceAll('{_val_}', obj[k][j])
+      html = html.replaceAll(`{${j}}`, obj[k][j])
+    }
+    htmls += html
+  }
+  return htmls
+}
 
+/////// Custom element
 w.element = (
   name,
   { onMount = x => x, beforeData = (x) => x, style, template = '' }
@@ -69,7 +84,6 @@ if (window.components) {
 }
 
 /////// State, data, and reactivity
-
 w.state = new Proxy({}, {
     set: (obj, prop, value) => {
       console.log(prop, value)
@@ -97,15 +111,10 @@ w.load = (name) => {
   return localStorage.getItem(name)
 }
 
-w.get = async (
-  url,
-  options = null,
-  transform = null,
-  key = 'temp'
-) => {
+w.get = async (url, options = {}, transform, key) => {
   let data = await (await fetch(`https://${url}`, options)).json()
   if (transform) data = transform(data)
-  state[key] = data
+  if (key) w.state[key] = data
   return data
 }
 
@@ -113,13 +122,12 @@ w.stream = async (url, key) => {
   const ev = new EventSource(url)
   ev.onmessage = (ev) => {
     const data = JSON.parse(ev.data)
-    w.state[key] = data
+    if (key) w.state[key] = data
     return data
   }
 }
 
 /////// Startup
-
 w.start = async () => {
   await w.ready();
   [...$$('*')].map(e => {
@@ -138,25 +146,18 @@ w.start = async () => {
     }
     let dta = e.attr?.data
     if (dta) {
-      if(dta.endsWith('[]')) {
-        dta = dta.replace('[]', '')
-      }
-      if (e.innerHTML && e.innerHTML.includes('{')) {
-        e.template = e.innerHTML.replaceAll('{', '${')
-        e.innerHTML = ''
+      console.log(`processing ${e}`)
+      if (e.innerHTML) {
+        e.template = e.innerHTML
+        if(e.innerHTML.match('{')) {
+          e.innerHTML = ''
+        }
       }
       e.set = (o) => {
-        e.innerHTML = ''
-        const template = e.template.replaceAll('{', '{rec.')
-        if (!Array.isArray(o)) o = [o]
-        const f = new Function('rec', 'return `' + template + '`')
-        o.map(rec => {
-            if(typeof rec !== 'object') rec = {value: rec}
-            e.innerHTML += f(rec)
-        })
+        e.innerHTML = w.flatten(o, e.template)
       }
       if (e.attr?.value) {
-        let o = e.attr.value
+         let o = e.attr.value
          try { o = JSON.parse(o) } catch(e) {} 
          w.state[dta] = o
       }
--- hance.mjs
+++
@@ -1,5 +0,0 @@
-export default function MyElement(value) {
-    return `
-        ${value}
-    `
-}
