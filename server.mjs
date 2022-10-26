b2273c078d51cd976658e5ff3c51b3e6a7af6329
CB <digplan@outlook.comm>
2022-10-25 21:43:58 -0500

    move tests to folder

--- server.mjs
+++
@@ -1,9 +0,0 @@
-import http from 'node:http'
-import { read } from 'instax'
-
-http.createServer(({url}, s) => {
-    console.log(url)
-    s.end(read(`.${url}`))
-}).listen(3000)
-
-console.log('started on 3000')
