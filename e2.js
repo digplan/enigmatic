b505218b12aea39954504adac298266564c805be
Chris Borkert <digplan@outlook.com>
2026-01-04 13:16:45 -0600

    Refactor custom element handling in e2.js and update version to 0.25.0 in package.json

--- e2.js
+++ e2.js
@@ -2,21 +2,27 @@ window.$  = document.querySelector.bind(document)
 window.$$ = document.querySelectorAll.bind(document)
 
 window.custom = {
-  "hello-world": (data) => ({
-    prop: () => `Hello ${data || 'World'}!`,
-    render: `Hello ${data || 'World'}!`
-  })
+  "hello-world": (data) => `Hello ${data}`,
+  "hello-world-2": {
+    prop: (data) => `${data} World`,
+    render: function(data) { 
+      return this.prop(data); 
+    }
+  }
 }
 
 window.state = new Proxy({}, {
   set(obj, prop, value) {
     obj[prop] = value
-    for (const e of $$(`[data="${prop}"]`)) {
-      const tag = e.tagName.toLowerCase()
-      if (custom[tag]) {
-        e.innerHTML = custom[tag](value).render
+    $$(`[data="${prop}"]`).forEach(el => {
+      console.log('setting', el.tagName);
+      const f = window.custom[el.tagName.toLowerCase()];
+      if(typeof f === 'function') {
+        el.innerHTML = f(value);
+      } else {
+        el.innerHTML = f.render(value);
       }
-    }
+    });
     return true
   }
 })
