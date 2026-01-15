91e248a9b672ecb6f5c20ca3fc7321965a40e8c3
Chris Borkert <digplan@outlook.com>
2026-01-14 19:07:43 -0600

    fix

--- server/cors.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PURGE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, Cookie, X-HTTP-Method-Override",
-  "Access-Control-Allow-Credentials": "true"
-}
